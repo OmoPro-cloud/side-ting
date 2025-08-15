10b6cbcbd0924dc1d3686268ac56946c51cf64b2
OmoPro-cloud <omopro@yahoo.com>
2025-08-15 17:50:25 -0500

    initial footballers dataframe commit

--- footballers_pandas.py
+++ footballers_pandas.py
@@ -2,3 +2,16 @@ import pandas as pd
 
 df = pd.read_csv('footballers.csv')
 print(df)
+print('-' * 30)
+
+#print whoever has more than 50 goals
+print(df[df['Goals'] > 50]) 
+print('-' * 30)
+
+#print whoever has more than 80 assists
+print(df[df['Assists'] > 80])
+print('-' * 30)
+
+#print whoever has more than 300 appearances
+print(df[df['Appearances'] > 300])
+print('-' * 30)
