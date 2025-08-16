77b989363ed6557ef5d13312b30bcff1b2d7f35b
OmoPro-cloud <omopro@yahoo.com>
2025-08-16 14:23:50 -0500

    Update footballers_pandas.py

--- footballers_pandas.py
+++ footballers_pandas.py
@@ -4,14 +4,16 @@ df = pd.read_csv('footballers.csv')
 print(df)
 print('-' * 30)
 
-#print whoever has more than 50 goals
-print(df[df['Goals'] > 50]) 
-print('-' * 30)
+#display shape and columns of dataframe
+print(df.shape)
+print(df.columns)
 
-#print whoever has more than 80 assists
-print(df[df['Assists'] > 80])
-print('-' * 30)
+#Filter through data
+#print people whose age is greater than 25
+print(df[df['Age'] > 25])
 
-#print whoever has more than 300 appearances
-print(df[df['Appearances'] > 300])
-print('-' * 30)
+#print people who;s age is greater than 20 and plays for arsenal
+print(df[df['Age'] > 25])
+
+#people whose age is greater than 25 and live in new york
+#print(df1[(df1['Age'] > 25) & (df1['City'] == 'Chicago')])
