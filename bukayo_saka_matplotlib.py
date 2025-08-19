24ad6f3eced7a30056a7cdea6fce0470c9874e7e
OmoPro-cloud <omopro@yahoo.com>
2025-08-19 17:42:23 -0500

    Update bukayo_saka_matplotlib.py

--- bukayo_saka_matplotlib.py
+++ bukayo_saka_matplotlib.py
@@ -12,14 +12,17 @@ ax.grid(True, alpha = 0.5)
 plt.show()
 '''
 
-fig, ax = plt.subplots()
-ax.plot(df['Year'], df['G+A'], marker='o', linewidth=2)
-ax.set(title='Bukayo Saka G+A in all seasons', xlabel='Year', ylabel='G+A')
-ax.grid(True, alpha=0.5)
-plt.show()
+#fig, ax = plt.subplots()
+#ax.plot(df['Year'], df['G+A'], marker='o', linewidth=2)
+#ax.set(title='Bukayo Saka G+A in all seasons', xlabel='Year', ylabel='G+A')
+#ax.grid(True, alpha=0.5)
+#plt.show()
 
 
 #all_stats = df.groupby('Year')['G+A'].max().reset_index()
 #overall_ga = all_stats.sort_values('G+A', ascending=False).head(5)
 #print('\nTop 5 Highest G+As recorded: ')
 #print(overall_ga)
+
+avg_ga = df['G+A'].mean()
+print('\n average g/a', avg_ga)
