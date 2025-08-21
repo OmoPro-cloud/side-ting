94d59af5974afb0d6da2864b1270dc56213a7085
OmoPro-cloud <omopro@yahoo.com>
2025-08-20 21:34:07 -0500

    Update weather_api_assignment_with_while_loop.py

--- weather_api_assignment_with_while_loop.py
+++ weather_api_assignment_with_while_loop.py
@@ -1,28 +1,24 @@
 import requests
 
 def get_weather(city, api_key):
-  while True:
-    try:
-      url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-      response = requests.get(url)
-      data = response.json()
-      if response.status_code != 200:
-        print('Error fetching weather', data.get('message', 'Unknown error'))
-        return
-      temp_k = data['main']['temp']
-      temp_c = temp_k - 273.15
-      weather_description = data['weather'][0]['description']
-      print(f'Weather in {city}: {weather_description}')
-      print(f'Temperature in {city}: {temp_c:.2f}')
-      return 'Done'
-    except Exception as e:
-      print('Erorr fetching weather: ', e)
+  url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+  response = requests.get(url)
+  data = response.json()
+  if response.status_code != 200:
+    print(f'Error fetching weather: ', data.get('weather', 'Unknown Error'))
+    return None
+  
+  temp_k = data['main']['temp']
+  temp_c = temp_c - 273.15
+  description = data['weather'][0]['description']
+  print(f'Weather in {city}: {description}')
+  print(f'Temperature in {city}: {temp_k:.2f}°C')
+  return True
 
-    city = input('Welcome to the Weather App. Please enter a city to view its weather' \
-    'or type "bye" to leave.')
-    if city == {'bye'}:
+def main(api_key):
+  while True:
+    city = input('Welcome to the Weather App. Enter a city (or type bye to exit).')
+    if city.lower() == 'bye':
+      print('Goodbye')
       break
-    weather = get_weather(city, api_key)
-    print(weather)
     
-print(get_weather)
