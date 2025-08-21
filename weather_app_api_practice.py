926c51e3835c9ee3224d9e1fc9028573e6f73204
OmoPro-cloud <omopro@yahoo.com>
2025-08-20 20:58:42 -0500

    Update weather_app_api_practice.py

--- weather_app_api_practice.py
+++ weather_app_api_practice.py
@@ -11,5 +11,16 @@ def get_weather(city, api_key):
     if response.status_code != 200: #if the request is NOT FOUND
       print('Error fetching weather', data.get('message', 'Unknown error'))
       return
+    temp_k = data['main']['temp']
+    temp_c = -273.15
+    weather_description = data['weather'][0]['description']
+    print(f'Weather in {city} right now: {weather_description}')
+    print(f'The temperature in {city} right now is: {temp_c:.2f}Celcius')
+    return 'Done'
   except Exception as e:
     print('Error fetching weather: ', e)
+  
+city = input('Enter your city: ')
+api_key = 'd445f540e455fccdf27aed42e74a6fd4'
+weather = get_weather(city, api_key)
+print(weather)
